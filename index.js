d1ee2dc1af23d2bc9b7c22a330aca17bf91a8d59
Ella R <ryukhinaella@gmail.com>
2022-10-15 23:48:29 -0700

    Render posts in js

--- index.js
+++ index.js
@@ -1,30 +1,76 @@
 const posts = [
     {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
+        name: "Daniella Haynes",
+        username: "olivia2331",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
+        avatar: "images/profile-1.jpg",
+        post: "images/ocean.jpg",
+        comment: "ocean always leaves a space for thought",
         likes: 21
     },
     {
-        name: "Gustave Courbet",
-        username: "gus1819",
+        name: "Samia Wilson",
+        username: "elisb9893",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        avatar: "images/profile-2.jpg",
+        post: "images/balance.jpg",
+        comment: "it's important to find a balance",
+        likes: 125
     },
     {
-        name: "Joseph Ducreux",
-        username: "jd1735",
+        name: "Brandon Bolton",
+        username: "mikh8736",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        avatar: "images/profile-3.jpg",
+        post: "images/work.jpg",
+        comment: "are you working today?",
+        likes: 151
     }
 ]
 
+const post = document.getElementById("main")
+
+
+
+let html = ''
+for (let i = 0; i < posts.length; i++) {
+    html += `
+<section class="each_post">
+    <div class="profile">
+        <img src="${posts[i].avatar}" alt="avatar_image" class="profile-image">
+        <p class="profile_data name">${posts[i].name}</p>
+        <span class="country">${posts[i].location}</span>
+    </div>
+        <img src="${posts[i].post}" alt="First post" class="post_image">
+    <div class="icons">
+        <img src="images/like.svg" alt="like_button"" class="icon heart">
+        <img src="images/icon-comment.png" alt="share_button" class="icon">
+        <img src="images/icon-dm.png" alt="comment_button" class="icon">
+    </div>
+        <p class="likes" id="like">${posts[i].likes} likes</p>
+        <p class="comment"><span class="name">${posts[i].username}</span> ${posts[i].comment}</p>
+</section>`
+}
+
+post.innerHTML = html
+
+
+
+
+const likeBtn = document.getElementsByClassName("heart")
+// likeBtn.addEventListener('click', () => {
+//     let currentLikes = document.getElementById("like")
+//     plusLike = Number(currentLikes.innerText.match(/\d+/g)) + 1
+//     currentLikes.textContent = `${plusLike} likes`
+// })
+
+// for (let i = 0; i < likeBtn.length; i++ ) {
+//     likeBtn[i].addEventListener("click", () => {
+//         let currentLikes = document.getElementById("like")
+//         likesUpdated()
+//     })
+// }
+
+// function likesUpdated(el, index){
+//     el.innerHTML = `${posts[index].likes} likes`
+// }
